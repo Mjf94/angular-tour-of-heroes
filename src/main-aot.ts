51b21c65fbdf808d5e7f73fc8d8865257007d936
Mike MA <594mjf@gmail.com>
2017-05-10 21:49:05 +0800

    Script Fix

--- src/main-aot.ts
+++ src/main-aot.ts
@@ -2,5 +2,5 @@
  * Created by mikema on 10/5/17.
  */
 import { platformBrowser } from '@angular/platform-browser';
-import { AppModuleNgFactory } from './aot/src/app/app.module.ngfactory';
+import {AppModuleNgFactory} from '../aot/src/app/app.module.ngfactory';
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
