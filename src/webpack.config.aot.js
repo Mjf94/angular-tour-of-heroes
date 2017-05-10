96020f79b2db006717364a3add121c7c4fe42ea0
Mike MA <594mjf@gmail.com>
2017-05-10 16:11:57 +0800

    AOT Configuration

---
+++ src/webpack.config.aot.js
@@ -0,0 +1,32 @@
+/**
+ * Created by mikema on 10/5/17.
+ */
+const ngtools = require('@ngtools/webpack');
+const webpack = require('webpack');
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    main: './src/main-aot.ts'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  target: 'node',
+  output: {
+    path: 'src/dist',
+    filename: 'build.js'
+  },
+  plugins: [
+    new ngtools.AotPlugin({
+      tsConfigPath: './tsconfig-aot.json'
+    }),
+    new webpack.optimize.UglifyJsPlugin({ sourceMap: true })
+  ],
+  module: {
+    rules: [
+      { test: /\.css$/, loader: 'raw-loader' },
+      { test: /\.html$/, loader: 'raw-loader' },
+      { test: /\.ts$/, loader: '@ngtools/webpack' }
+    ]
+  }
+}
