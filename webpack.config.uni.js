4217ee301b367f94105b0eb9df66142585d46d39
Mike MA <594mjf@gmail.com>
2017-05-10 18:24:22 +0800

    Universal Works

---
+++ webpack.config.uni.js
@@ -0,0 +1,34 @@
+/**
+ * Created by mikema on 10/5/17.
+ */
+const ngtools = require('@ngtools/webpack');
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    main: ['./src/uni/app.server.ts', './src/uni/server-aot.ts']
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  target: 'node',
+  output: {
+    path: path.resolve(__dirname, 'src/dist'),
+    filename: 'server.js'
+  },
+  plugins: [
+    new ngtools.AotPlugin({
+      tsConfigPath: './tsconfig-aot.json'
+    }),
+    new webpack.optimize.UglifyJsPlugin({ sourceMap: true })
+  ],
+  module: {
+    rules: [
+      { test: /\.css$/, loader: 'raw-loader' },
+      { test: /\.html$/, loader: 'raw-loader' },
+      { test: /\.ts$/, loader: '@ngtools/webpack' }
+    ]
+  }
+}
